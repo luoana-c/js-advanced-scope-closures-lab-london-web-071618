2b44f757bcfaa3e286ace6e35ec14a1c142d7e97
Luoana Chirita <luoana.livadariu@gmail.com>
2018-08-30 13:07:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function produceDrivingRange(blockRange) {
   return function (blockOne, blockTwo) {
     const blockDiff = blockTwo.substring(0, 2) - blockOne.substring(0, 2)
+    console.log(blockTwo.substring(0, 2))
     if (blockDiff > blockRange) {
       return `${blockDiff} blocks out of range`
     }
